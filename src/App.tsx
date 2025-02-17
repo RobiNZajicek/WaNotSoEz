f722d651e0478c7f5c0e54a809e5c37d7967155c
Robin Zajicek <zajicekrobin3@gmail.com>
2025-02-17 14:14:52 +0100

    passwod compoent very very good

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react';
+import PasswordInput from './components/PasswordInput';
+import PasswordStrength from './components/PasswordStrength';
 
-const App = () => {
+function App() {
+  const [password, setPassword] = useState('');
+
   return (
-    <div>
-      <h1>Ahoj</h1>
+    <div style={{ margin: '20px', maxWidth: '400px' }}>
+      <h1>Hodnocení síly hesla</h1>
+      <PasswordInput setPassword={setPassword} />
+      <PasswordStrength password={password} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
